1e8a48f42ea938a8ce06fe49e39bdcb4147362ac
John Meyer <john32meyer@gmail.com>
2021-04-25 21:26:37 -0700

    Fixed HW 1

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -2,8 +2,12 @@ var num1 = 5;
 var num2 = 7;
 
 var sum = num1 + num2;
-var msg = (num1 + ' + ' + num2 + ' is : ' + sum);
+var msg = ('The sum of ' + num1 + ' + ' + num2 + ' is : ' + sum);
 console.log(msg);
 
 var eqel = document.getElementById('item');
 eqel.textContent = msg;
+
+var titleText = 'Welcome to my calculator';
+var tiel = document.getElementById('title');
+tiel.textContent = titleText;
