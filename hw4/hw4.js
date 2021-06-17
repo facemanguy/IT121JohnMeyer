b4e5a7094e858511610058229b2a1a6eed2aae7e
John Meyer <john32meyer@gmail.com>
2021-06-16 22:50:35 -0700

    Removed the fun from hw4

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -5,7 +5,7 @@ var input;
 
 do{
     //Prompt user for entries at least once
-    input = parseInt(prompt("Average Calculator!\n- Enter a whole number\n- Enter \"999\" to calculate the average of all entries"));
+    input = parseInt(prompt("Enter Test Score\nOr enter 999 to end entries"));
     //Add values to total and count for the calculation
     inputTotal += input;
     valueCount++;
@@ -17,7 +17,7 @@ do{
 if (isNaN(average)){
     alert("Oops, something wasn't a valid number\nRefresh and try again");
 }else{
-    alert("The average of your entries is " + average + "!\nRefresh the page to start a new calculation");
+    alert("Average score is " + average);
 }
 /*
 I think you could use local or session storage to store the values from multiple calculations.
